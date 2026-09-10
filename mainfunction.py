3a83f8245dc97949e7211f66c667cc924bf0994a
Tia <tiapriyel@icloud.com>
2026-09-10 12:17:46 +0200

    Add files via upload

---
+++ mainfunction.py
@@ -0,0 +1,11 @@
+from main import InvoiceGenerator
+from tkinter import Tk
+
+def main():
+   root = Tk()
+   obj = InvoiceGenerator(root)
+   root.mainloop()
+
+if __name__ == "__main__":
+   
+   main()
